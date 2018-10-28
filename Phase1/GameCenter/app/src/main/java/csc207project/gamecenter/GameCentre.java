47c6d2972b556fdc32dc7e1c5fb7044288f74d18
Weiqing Wang <weiqing.wang@mail.utoronto.ca>
2018-10-28 14:34:23 -0400

    added signin, signup listener

--- Phase1/GameCenter/app/src/main/java/csc207project/gamecenter/GameCentre.java
+++ Phase1/GameCenter/app/src/main/java/csc207project/gamecenter/GameCentre.java
@@ -2,6 +2,8 @@ package csc207project.gamecenter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.net.PasswordAuthentication;
@@ -10,6 +12,9 @@ public class GameCentre extends AppCompatActivity {
 
     private TextView username;
     private TextView password;
+    private Button signInButton;
+    private Button signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +22,33 @@ public class GameCentre extends AppCompatActivity {
         setContentView(R.layout.activity_game_centre);
         username = (TextView) findViewById(R.id.userName);
         password = (TextView) findViewById(R.id.Password);
+
         String name = username.getText().toString();
         String pw = password.getText().toString();
-
-
+        addSignInButtonListener();
+        addSignUpButtonListener();
     }
+
+    private void addSignUpButtonListener() {
+        signUpButton = (Button) findViewById(R.id.SignUpButton);
+        signUpButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }));
+    }
+
+    private void addSignInButtonListener() {
+        signInButton = (Button) findViewById(R.id.SignInButton);
+        signInButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        })
+        );
+    }
+
+
 }
