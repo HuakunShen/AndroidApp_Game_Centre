ab698accdeec6ed3c03f654a14be8bf1779500fb
Weiqing Wang <weiqing.wang@mail.utoronto.ca>
2018-10-28 22:54:50 -0400

    Created the account registration layout.

---
+++ Phase1/GameCenter/app/src/main/java/csc207project/gamecenter/AccountRegistration.java
@@ -0,0 +1,13 @@
+package csc207project.gamecenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_registration);
+    }
+}
