2da306e04d5beaf0ca37e8d6cc91601376ff6264
Jato Xiang <jiatao.xiang@mail.utoronto.ca>
2018-10-29 22:49:59 -0400

    add AutoSave Interface

---
+++ Phase1/GameCenter/app/src/main/java/csc207project/gamecenter/AutoSave/AutoSave.java
@@ -0,0 +1,21 @@
+package csc207project.gamecenter.AutoSave;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public interface AutoSave {
+
+    int getTimeInterval();
+
+    default void setTimer(){
+        Timer t = new Timer("AutoSaveTimer");
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                saveToFile();
+            }
+        },getTimeInterval(), getTimeInterval());
+    }
+
+    void saveToFile();
+
+}
