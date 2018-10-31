b4fafa4eee547341baead9dfe7ce7daab5594837
Yuan Xu <xuyuan.xu@mail.utoronto.ca>
2018-10-31 01:26:40 -0400

    add import image to NewGame

--- Phase1/GameCenter/app/src/main/java/csc207project/gamecenter/SlidingTiles/NewGameActivity.java
+++ Phase1/GameCenter/app/src/main/java/csc207project/gamecenter/SlidingTiles/NewGameActivity.java
@@ -2,10 +2,13 @@ package csc207project.gamecenter.SlidingTiles;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.net.Uri;
+import android.widget.ImageView;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -29,14 +32,22 @@ public class NewGameActivity extends AppCompatActivity {
      */
     private BoardManager boardManager;
 
+    /**
+     * Variable to store Uri of imported image.
+     */
+    private static final int SELECT_IMAGE = 1801;
+    Uri imageUri;
+    ImageView image_selected;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
 
         boardManager = new BoardManager();
-
+        image_selected = (ImageView) findViewById(R.id.newgame_im_preview);
         addStartButtonListener();
+        addImportButtonListener();
     }
 
     /**
@@ -47,15 +58,39 @@ public class NewGameActivity extends AppCompatActivity {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //boardManager = new BoardManager();
-                //switchToGame();
-
                 saveToFile(TEMP_SAVE_FILENAME);
                 Intent to_game = new Intent(NewGameActivity.this, GameActivity.class);
                 startActivity(to_game);
             }
         });
     }
+
+    private void addImportButtonListener() {
+        Button startButton = findViewById(R.id.newgame_import);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+
+    private void openGallery() {
+        Intent get_photo = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(get_photo, SELECT_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SELECT_IMAGE && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+            //TODO we need to chop this photo to be a square
+            image_selected.setImageURI(imageUri);
+        }
+    }
+
 
     public void saveToFile(String fileName) {
         try {
