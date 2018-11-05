2e2952e945c7adb78b91bb6032a4a58776530610
Xu Wang <xuxu.wang@mail.utoronto.ca>
2018-11-04 23:20:41 -0500

    complete the scoreboard ui

--- Phase1/GameCenter/app/src/main/java/csc207project/gamecenter/ScoreBoard/ScoreBoardActivity.java
+++ Phase1/GameCenter/app/src/main/java/csc207project/gamecenter/ScoreBoard/ScoreBoardActivity.java
@@ -3,29 +3,37 @@ package csc207project.gamecenter.ScoreBoard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import csc207project.gamecenter.R;
 
 public class ScoreBoardActivity extends AppCompatActivity {
 
-    private LinearLayout scoreboardDisplay;
+    private TableLayout scoreboardTable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_board);
-        scoreboardDisplay = findViewById(R.id.scoreboardDisplay);
+        scoreboardTable = findViewById(R.id.scoreboardTable);
         addText();
+
     }
 
     private void addText() {
-        int N = 10;
-//        TextView[] textViews = new TextView[N];
-        for(int i = 0; i < 10; i++){
-            TextView thisTextView = new TextView(this);
-            thisTextView.setTextSize(30);
-            thisTextView.setText("This is row # " + i);
-            scoreboardDisplay.addView(thisTextView);
+        TableRow row = new TableRow(this);
+        for(int i = 0; i < 4; i++){
+            TextView text = new TextView(this);
+            text.setText(((Integer) i).toString());
+            if(i == 2) {
+                text.setWidth(90);
+            }else{
+                text.setWidth((100));
+            }
+            text.setGravity(17);
+            row.addView(text);
         }
+        scoreboardTable.addView(row);
     }
 }
