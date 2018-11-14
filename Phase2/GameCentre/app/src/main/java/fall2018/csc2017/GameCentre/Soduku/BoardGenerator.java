ba487f1a9008d0c7050c6a0a5c964e797901a8c2
Weiqing Wang <weiqing.wang@mail.utoronto.ca>
2018-11-14 00:40:38 -0500

    Finished the BoardGenerator

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/Soduku/BoardGenerator.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/Soduku/BoardGenerator.java
@@ -5,19 +5,38 @@ import java.util.Random;
 
 public class BoardGenerator {
 
-    public Integer[][] getBoard() {
-        return board;
-    }
+    /**
+     * The board
+     */
+    private Integer[][] board = new Integer[9][9];
 
-    public Integer[][] board = new Integer[9][9];
-
-    public BoardGenerator() {
+    /**
+     * Constructor for the BoardGenerator Class.
+     */
+    BoardGenerator() {
         boardInitializer();
-        juniorBoardShuffler(1);
-        juniorBoardShuffler(0);
+        for (int n = 0; n < 100; n++) {
+            seniorBoardShuffler(0);
+            juniorBoardShuffler(0);
+        }
+        for (int n = 0; n < 100; n++) {
+            seniorBoardShuffler(1);
+            juniorBoardShuffler(1);
+        }
+
         System.out.println(Arrays.deepToString(board));
     }
 
+    /**
+     * Getter function for the board.
+     */
+    Integer[][] getBoard() {
+        return board;
+    }
+
+    /**
+     * Initialize a board that satisfies the Sudoku requirements.
+     */
     private void boardInitializer() {
         int boxValue;
         int firstBoxValue = 1;
@@ -41,6 +60,9 @@ public class BoardGenerator {
         }
     }
 
+    /**
+     * Shuffle the game board.
+     */
     private void juniorBoardShuffler(int check) {
         int k1, k2;
         int max = 2;
@@ -60,62 +82,37 @@ public class BoardGenerator {
         }
     }
 
-    public void seniorBoardShuffler(int k1, int k2) {
-        int row_from;
-        int row_to;
-        int col_from;
-        int col_to;
-        int i, j, b, c;
-        int rem1, rem2;
-        int flag;
-        int count = 9;
-        for (i = 1; i <= 9; i++) {
-            flag = 1;
-            for (j = 0; j < 9; j++) {
-                if (j != k2) {
-                    if (i == board[k1][j]) {
-                        flag = 0;
-                        break;
-                    }
+    /**
+     * Further Shuffle the GameBoard.
+     */
+    private void seniorBoardShuffler(int check) {
+        Random r = new Random();
+        int k1;
+        int k2;
+        for (int n = 0; n < 100; n++) {
+            if (check == 0) {
+                k1 = r.nextInt(3) + 1;
+                k2 = r.nextInt(3) + 1;
+                while (k1 == k2) {
+                    k1 = r.nextInt(3) + 1;
+                    k2 = r.nextInt(3) + 1;
                 }
-            }
-            if (flag == 1) {
-                for (c = 0; c < 9; c++) {
-                    if (c != k1) {
-                        if (i == board[c][k2]) {
-                            flag = 0;
-                            break;
-                        }
-                    }
+                switchVerticalGroups(k1, k2);
+            } else if (check == 1) {
+                k1 = r.nextInt(3) + 1;
+                k2 = r.nextInt(3) + 1;
+                while (k1 == k2) {
+                    k1 = r.nextInt(3) + 1;
+                    k2 = r.nextInt(3) + 1;
                 }
+                switchHorizontalGroups(k1, k2);
             }
-            if (flag == 1) {
-                rem1 = k1 % 3;
-                rem2 = k2 % 3;
-                row_from = k1 - rem1;
-                row_to = k1 + (2 - rem1);
-                col_from = k2 - rem2;
-                col_to = k2 + (2 - rem2);
-                for (c = row_from; c <= row_to; c++) {
-                    for (b = col_from; b <= col_to; b++) {
-                        if (c != k1 && b != k2) {
-                            if (i == board[c][b]) {
-                                flag = 0;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            if (flag == 0)
-                count--;
-        }
-        if (count == 1) {
-            board[k1][k2] = 0;
         }
     }
 
-
+    /**
+     * Switch two rows on the game board.
+     */
     private void switchRows(int row1, int row2) {
         int cache;
         for (int columnIndex = 0; columnIndex < 9; columnIndex++) {
@@ -125,6 +122,9 @@ public class BoardGenerator {
         }
     }
 
+    /**
+     * Switch two columns on the game board.
+     */
     private void switchColumns(int col1, int col2) {
         int cache;
         for (int rowIndex = 0; rowIndex < 9; rowIndex++) {
@@ -134,29 +134,39 @@ public class BoardGenerator {
         }
     }
 
-    public void switchHorizontalGroups(int group1, int group2) {
+    /**
+     * Switch two groups of rows in the game board.
+     */
+    private void switchHorizontalGroups(int group1, int group2) {
+        int row1 = 3 * group1 - 3;
+        int row2 = 3 * group2 - 3;
         int cache;
         for (int n = 1; n <= 3; n++) {
             for (int columnIndex = 0; columnIndex < 9; columnIndex++) {
-                cache = board[group1][columnIndex];
-                board[group1][columnIndex] = board[group2][columnIndex];
-                board[group2][columnIndex] = cache;
+                cache = board[row1][columnIndex];
+                board[row1][columnIndex] = board[row2][columnIndex];
+                board[row2][columnIndex] = cache;
             }
-            group1++;
-            group2++;
+            row1++;
+            row2++;
         }
     }
 
-    public void switchVerticalGroups(int group1, int group2) {
+    /**
+     * Switch two groups of columns in the game board.
+     */
+    private void switchVerticalGroups(int group1, int group2) {
+        int row1 = 3 * group1 - 3;
+        int row2 = 3 * group2 - 3;
         int cache;
         for (int n = 1; n <= 3; n++) {
             for (int rowIndex = 0; rowIndex < 9; rowIndex++) {
-                cache = board[rowIndex][group1];
-                board[rowIndex][group1] = board[rowIndex][group2];
-                board[rowIndex][group2] = cache;
+                cache = board[rowIndex][row1];
+                board[rowIndex][row1] = board[rowIndex][row2];
+                board[rowIndex][row2] = cache;
             }
-            group1++;
-            group2++;
+            row1++;
+            row2++;
         }
     }
 }
