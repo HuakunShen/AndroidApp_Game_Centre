0c04334b140dce5737664f4347c24196f8d257db
Huakun Shen <huakun.shen@mail.utoronto.ca>
2018-11-16 00:26:41 -0500

    add sliding tiles original boardManager unit test to our test. Modified A little bit, because Board class has static variables NUM_COLS and NUM_ROWS, the original test from sliding tiles is designed for 4x4 Board and would fail if we use exactly the same code. Now I had to specify that the test case has 4x4 board in order to pass the test.

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/BoardAndTileTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/BoardAndTileTest.java
@@ -26,6 +26,70 @@ public class BoardAndTileTest {
     private BoardManager boardManager;
 
     /**
+     * Make a solved Board.
+     */
+    private void setUpCorrect() {
+        List<Tile> tiles = makeTiles();
+        Board board = new Board(tiles);
+        boardManager = new BoardManager(board);
+    }
+
+
+    /**
+     * Test whether swapping two tiles makes a solved board unsolved.
+     */
+    @Test
+    public void testIsSolved() {
+        setUpCorrect();
+        assertEquals(true, boardManager.puzzleSolved());
+        swapFirstTwoTiles();
+        assertEquals(false, boardManager.puzzleSolved());
+    }
+
+    /**
+     * Test whether swapping the first two tiles works.
+     */
+    @Test
+    public void testSwapFirstTwo() {
+        setUpCorrect();
+        assertEquals(1, boardManager.getBoard().getTile(0, 0).getId());
+        assertEquals(2, boardManager.getBoard().getTile(0, 1).getId());
+        boardManager.getBoard().swapTiles(0, 0, 0, 1);
+        assertEquals(2, boardManager.getBoard().getTile(0, 0).getId());
+        assertEquals(1, boardManager.getBoard().getTile(0, 1).getId());
+    }
+
+    /**
+     * Test whether swapping the last two tiles works.
+     */
+    @Test
+    public void testSwapLastTwo() {
+        Board.NUM_ROWS = 4;
+        Board.NUM_COLS = 4;
+        setUpCorrect();
+        assertEquals(15, boardManager.getBoard().getTile(3, 2).getId());
+        assertEquals(16, boardManager.getBoard().getTile(3, 3).getId());
+        boardManager.getBoard().swapTiles(3, 3, 3, 2);
+        assertEquals(16, boardManager.getBoard().getTile(3, 2).getId());
+        assertEquals(15, boardManager.getBoard().getTile(3, 3).getId());
+    }
+
+    /**
+     * Test whether isValidHelp works.
+     */
+    @Test
+    public void testIsValidTap() {
+        Board.NUM_ROWS = 4;
+        Board.NUM_COLS = 4;
+        setUpCorrect();
+        assertEquals(true, boardManager.isValidTap(11));
+        assertEquals(true, boardManager.isValidTap(14));
+        assertEquals(false, boardManager.isValidTap(10));
+    }
+
+
+
+    /**
      * Generate an ArrayList of random tiles by the size.
      */
     private ArrayList<Integer> generateRandomNumber(int size){
@@ -173,6 +237,27 @@ public class BoardAndTileTest {
         setUpCorrectFourByFour(listOfTile);
         assertEquals(2, boardManager.blankPosition());
     }
+
+    private List<Tile> makeTiles() {
+        List<Tile> tiles = new ArrayList<>();
+        final int numTiles = Board.NUM_ROWS * Board.NUM_COLS;
+        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+            tiles.add(new Tile(tileNum + 1, tileNum));
+        }
+
+        return tiles;
+    }
+
+
+
+    /**
+     * Shuffle a few tiles.
+     */
+    private void swapFirstTwoTiles() {
+        boardManager.getBoard().swapTiles(0, 0, 0, 1);
+    }
+
+
 
 }
 
