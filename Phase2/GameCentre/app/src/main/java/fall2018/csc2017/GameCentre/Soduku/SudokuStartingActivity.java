27daba1e467a83f041d2fecdd558df6be16efb48
Huakun Shen <huakun.shen@mail.utoronto.ca>
2018-11-17 15:56:42 -0500

    add startingActivity and gameActivity to Sudoku

---
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/Soduku/SudokuStartingActivity.java
@@ -0,0 +1,15 @@
+package fall2018.csc2017.GameCentre.Soduku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fall2018.csc2017.GameCentre.R;
+
+public class SudokuStartingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sudoku_starting);
+    }
+}
