e36d377eeb28d72ef56ed9f626617fe329f0b2a7
Yuan Xu <xuyuan.xu@mail.utoronto.ca>
2018-11-25 03:30:44 -0400

    added image button: scoreboard

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/slidingTiles/SlidingTilesStartingActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/slidingTiles/SlidingTilesStartingActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
@@ -135,7 +136,7 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
     }
 
     private void addScoreboardButtonListener() {
-        Button scoreboardButton = findViewById(R.id.scoreboardButton);
+        ImageButton scoreboardButton = findViewById(R.id.scoreboardButton);
         scoreboardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
