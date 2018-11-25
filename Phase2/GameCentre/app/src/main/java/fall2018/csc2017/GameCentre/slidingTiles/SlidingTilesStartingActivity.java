a2fcfb27dbbdeed4f20f35bf941926b84e06e8ea
Yuan Xu <xuyuan.xu@mail.utoronto.ca>
2018-11-25 01:38:04 -0400

    cleared code in activity SlidingTilesStartingActivity

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/slidingTiles/SlidingTilesStartingActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/slidingTiles/SlidingTilesStartingActivity.java
@@ -36,7 +36,6 @@ import static android.graphics.Bitmap.createBitmap;
  */
 public class SlidingTilesStartingActivity extends AppCompatActivity {
 
-    private static final int MAX_UNDO_LIMIT = 20;
     private User user;
     private String username;
     private String userFile;
@@ -55,23 +54,12 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
     public static final String GAME_NAME = "SlidingTiles";
     private SlidingTilesBoardManager boardManager;
 
-    private String[] list_diff = new String[]{"Easy(3x3)", "Normal(4x4)", "Hard(5x5)"};
-    private int selected_difficulty;
-
-    private static final int SELECT_IMAGE_CODE = 1801;
-    ImageButton importButton;
-    Bitmap bitmapCut;
-    public static Bitmap[] tileImages3x3 = new Bitmap[9];
-    public static Bitmap[] tileImages4x4 = new Bitmap[16];
-    public static Bitmap[] tileImages5x5 = new Bitmap[25];
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         db = new DatabaseHandler(this);
         username = getIntent().getStringExtra("user");
-        selected_difficulty = 4;
 
         setupUser();
         setupFile();
@@ -83,9 +71,6 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
         addStartButtonListener();
         addLoadButtonListener();
         addSaveButtonListener();
-        addDiffSpinnerListener();
-        addImportButtonListener();
-        addRadioButtonListener();
     }
 
     /**
@@ -96,7 +81,6 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
         username = getIntent().getStringExtra("user");
         userFile = db.getUserFile(username);
         loadFromFile(userFile);
-//        Toast.makeText(this, "Welcome " + user.getUsername(), Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -117,52 +101,15 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
      */
     private void addStartButtonListener() {
         Button startButton = findViewById(R.id.NewGameButton);
-        final EditText undoLimit = findViewById(R.id.undoLimitInput);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent tmp = new Intent(SlidingTilesStartingActivity.this, SlidingTilesNewGamePop.class);
                 saveToFile(tempGameStateFile);
                 tmp.putExtra("user", username);
                 startActivity(tmp);
-//                if (importButton.getVisibility() == View.VISIBLE && tileImages3x3[0] == null) {
-//                    Toast.makeText(SlidingTilesStartingActivity.this,
-//                            "You need to import image!", Toast.LENGTH_SHORT).show();
-//                } else {
-//                    boardManager = new SlidingTilesBoardManager(selected_difficulty);
-//                    if (setUndoSteps(undoLimit))
-//                        switchToGame();
-//                }
-
             }
         });
-    }
-
-    /**
-     * set up undo limit for steps
-     *
-     * @param undoLimit
-     * @return
-     */
-    public boolean setUndoSteps(EditText undoLimit) {
-        String inputStr = undoLimit.getText().toString();
-
-        int input;
-        if (inputStr.matches("")) {
-            input = 3;
-        } else {
-            input = Integer.parseInt(inputStr);
-        }
-
-        if (input > MAX_UNDO_LIMIT) {
-            Toast.makeText(this, "Exceeds Undo Limit: " +
-                    MAX_UNDO_LIMIT, Toast.LENGTH_SHORT).show();
-            return false;
-        } else {
-            boardManager.setCapacity(input);
-            return true;
-        }
     }
 
     /**
@@ -173,15 +120,10 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (importButton.getVisibility() == View.VISIBLE && tileImages3x3[0] == null) {
-                    Toast.makeText(SlidingTilesStartingActivity.this,
-                            "You need to import image!", Toast.LENGTH_SHORT).show();
-                } else {
-                    loadFromFile(gameStateFile);
-                    saveToFile(tempGameStateFile);
-                    makeToast("Loaded Game");
-                    switchToGame();
-                }
+                loadFromFile(gameStateFile);
+                saveToFile(tempGameStateFile);
+                makeToast("Loaded Game");
+                switchToGame();
             }
         });
     }
@@ -200,107 +142,6 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
             }
         });
     }
-
-    /**
-     * Activate the spinner for selecting difficulty.
-     */
-    private void addDiffSpinnerListener() {
-        Spinner select_diff = findViewById(R.id.list_diff_sele);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, list_diff);
-        select_diff.setAdapter(arrayAdapter);
-
-        select_diff.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (parent.getItemAtPosition(position) == list_diff[0]) {
-                    selected_difficulty = 3;
-                } else if (parent.getItemAtPosition(position) == list_diff[1]) {
-                    selected_difficulty = 4;
-                } else if (parent.getItemAtPosition(position) == list_diff[2]) {
-                    selected_difficulty = 5;
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                selected_difficulty = 4;
-            }
-        });
-    }
-
-    /**
-     * Activate the import ImageButton.
-     */
-    private void addImportButtonListener() {
-        importButton = findViewById(R.id.select_image);
-        importButton.setVisibility(View.INVISIBLE);
-        importButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openGallery();
-            }
-        });
-    }
-
-
-    /**
-     * open gallery for image selection
-     */
-    private void openGallery() {
-        Intent get_photo = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(get_photo, SELECT_IMAGE_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == SELECT_IMAGE_CODE && resultCode == RESULT_OK) {
-            Uri imageUri = data.getData();
-
-            try {
-                FileInputStream image_fis = new FileInputStream(getContentResolver().openFileDescriptor(imageUri, "r").getFileDescriptor());
-                Bitmap bitmapUncut = BitmapFactory.decodeStream(image_fis);
-                bitmapCut = cutToTileRatio(bitmapUncut);
-                importButton.setImageBitmap(bitmapCut);
-
-                cutImageToTiles();
-            } catch (FileNotFoundException e) {
-                Toast.makeText(this, "File does not exist", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    /**
-     * Activate the radio buttons.
-     */
-    private void addRadioButtonListener() {
-        RadioButton withImageButton = findViewById(R.id.withImageButton);
-        RadioButton withNumberButton = findViewById(R.id.withNumberButton);
-        withNumberButton.setChecked(true);
-        withImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(SlidingTilesStartingActivity.this, "With Image Selected",
-                        Toast.LENGTH_SHORT).show();
-                if (bitmapCut != null && tileImages3x3[0] == null) {
-                    cutImageToTiles();
-                }
-                importButton.setVisibility(View.VISIBLE);
-            }
-        });
-        withNumberButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(SlidingTilesStartingActivity.this, "With Number Selected",
-                        Toast.LENGTH_SHORT).show();
-                clearImages();
-                importButton.setVisibility(View.INVISIBLE);
-            }
-        });
-    }
-
-
 
     private void makeToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
@@ -370,57 +211,5 @@ public class SlidingTilesStartingActivity extends AppCompatActivity {
         } catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
         }
-    }
-
-    public Bitmap cutToTileRatio(Bitmap bitmapUncut) {
-        int width = bitmapUncut.getWidth();
-        int height = bitmapUncut.getHeight();
-        if (width * 320 > height * 250) {
-            int x = (width - height * 250 / 320) / 2;
-            return createBitmap(bitmapUncut, x, 0, width - 2 * x, height, null, false);
-        } else if (width * 320 < height * 250) {
-            int y = (height - width * 320 / 250) / 2;
-            return createBitmap(bitmapUncut, 0, y, width, height - 2 * y, null, false);
-        } else {
-            return bitmapUncut;
-        }
-    }
-
-    private void cutImageToTiles() {
-        int width = bitmapCut.getWidth();
-        int height = bitmapCut.getHeight();
-
-        int count = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                tileImages3x3[count] = createBitmap(bitmapCut, i * (width / 3),
-                        j * (height / 3), width / 3, height / 3, null, false);
-                count++;
-            }
-        }
-        count = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                tileImages4x4[count] = createBitmap(bitmapCut, i * (width / 4),
-                        j * (height / 4), width / 4, height / 4, null, false);
-                count++;
-            }
-        }
-        count = 0;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                tileImages5x5[count] = createBitmap(bitmapCut, i * (width / 5),
-                        j * (height / 5), width / 5, height / 5, null, false);
-                count++;
-            }
-        }
-    }
-
-
-    public void clearImages() {
-        tileImages3x3 = new Bitmap[9];
-        tileImages4x4 = new Bitmap[16];
-        tileImages5x5 = new Bitmap[25];
-        importButton.setImageDrawable(getResources().getDrawable(R.drawable.import_image));
     }
 }
