39577d473a6d7ce82fc708436a5418448606eab0
Xu Wang <xuxu.wang@mail.utoronto.ca>
2018-11-25 16:07:56 -0500

    Starting on MatchingBoardManagerTest

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/pictureMatching/MatchingBoardManagerTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/pictureMatching/MatchingBoardManagerTest.java
@@ -1,26 +1,54 @@
 package fall2018.csc2017.GameCentre.pictureMatching;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class MatchingBoardManagerTest {
 
+    MatchingBoardManager manager;
+
+    @Before
+    public void setUp() throws Exception {
+        this.manager = new MatchingBoardManager();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+//    @Test
+//    public addPictureTileToList(){
+//
+//    }
+
     @Test
-    public void getBoard() {
+    public void testGetBoard() {
     }
 
     @Test
-    public void puzzleSolved() {
+    public void testGetDifficulty() {
     }
 
     @Test
-    public void isValidTap() {
+    public void testGetTimeTaken() {
     }
 
     @Test
-    public void cancelPicture() {
+    public void testSetTimeTaken() {
     }
 
     @Test
-    public void getDifficulty() {
+    public void testBoardSolved() {
+    }
+
+    @Test
+    public void testIsValidTap() {
+    }
+
+    @Test
+    public void testMakeMove() {
     }
 }
