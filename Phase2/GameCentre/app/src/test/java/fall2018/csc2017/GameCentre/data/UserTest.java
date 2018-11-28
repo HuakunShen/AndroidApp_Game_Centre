09a465f7378da3a7ec5ad9f56d7f0d5554221052
Huakun Shen <huakun.shen@mail.utoronto.ca>
2018-11-28 13:08:18 -0500

    Completed User class unit test, but since there are many getters and setters, the test coverage for this class is 67%

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/data/UserTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/data/UserTest.java
@@ -4,6 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class UserTest {
@@ -11,6 +14,7 @@ public class UserTest {
     @Before
     public void setUp() throws Exception {
         user = new User("username", "password");
+        user.updateScore("game", 100);
     }
 
     @After
@@ -26,21 +30,35 @@ public class UserTest {
         assertFalse(result);
         result = user.checkPassword("newPassword");
         assertTrue(result);
+
     }
 
     @Test
     public void updateScore() {
+        assertEquals(user.getScore("game"), 100);
+        assertEquals(user.getScore("game1"), -1);
     }
 
     @Test
     public void getFile() {
+        assertEquals(user.getFile("game"), "username_game_data.ser");
+        assertEquals(user.getFile("game1"), "DNE");
     }
 
     @Test
     public void getScore() {
+
     }
 
     @Test
     public void getScoreboardData() {
+        List<List<String>> dataList = new ArrayList<>();
+        List<String> data = new ArrayList<>();
+        data.add("game");
+        data.add("username");
+        data.add("100");
+        dataList.add(data);
+        assertEquals(dataList, user.getScoreboardData());
+
     }
 }
