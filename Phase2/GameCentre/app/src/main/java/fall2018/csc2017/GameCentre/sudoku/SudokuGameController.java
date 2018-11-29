75d53a45ae18d206f50110fb8225f1021d9a7e37
Xu Wang <xuxu.wang@mail.utoronto.ca>
2018-11-29 00:41:20 -0500

    Added docString for SudokuGameController class

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/sudoku/SudokuGameController.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/sudoku/SudokuGameController.java
@@ -19,20 +19,58 @@ import fall2018.csc2017.GameCentre.data.User;
 import static android.content.Context.MODE_PRIVATE;
 
 public class SudokuGameController {
+    /**
+     * The context.
+     */
     private final Context context;
+    /**
+     * The database.
+     */
     private SQLDatabase db;
+    /**
+     * The user.
+     */
     private User user;
+    /**
+     * The gameStateFile.
+     */
     private String gameStateFile;
+    /**
+     * The gameRunning.
+     */
     private boolean gameRunning;
+    /**
+     * The tempGameStateFile.
+     */
     private String tempGameStateFile;
+    /**
+     * The SudokuBoardManager.
+     */
     private SudokuBoardManager boardManager;
-
+    /**
+     * The game name.
+     */
     private static final String GAME_NAME = "Sudoku";
-
+    /**
+     * The list of cell buttons.
+     */
     private List<Button> cellButtons;
+    /**
+     * The resource.
+     */
     private Resources resources;
+    /**
+     * The packageName.
+     */
     private String packageName;
 
+
+    /**
+     * Constructor for SudokuGameController.
+     *
+     * @param context
+     * @param user
+     */
     SudokuGameController(Context context, User user){
         this.context = context;
         this.db = new SQLDatabase(context);
@@ -41,46 +79,109 @@ public class SudokuGameController {
         this.resources = context.getResources();
     }
 
+    /**
+     * Return list of buttons.
+     *
+     * @return list of buttons
+     */
     public List<Button> getCellButtons() {
         return cellButtons;
     }
+
+    /**
+     * Return the user's file
+     *
+     * @return user's file
+     */
     public String getUserFile(){
         return db.getUserFile(user.getUsername());
     }
+
+    /**
+     * Return whether game is still running.
+     *
+     * @return whether game is still running
+     */
     public boolean isGameRunning() {
         return gameRunning;
     }
+
+    /**
+     * Set gameRunning.
+     *
+     * @param gameRunning
+     */
     public void setGameRunning(boolean gameRunning) {
         this.gameRunning = gameRunning;
     }
 
+    /**
+     * Set boardManager.
+     *
+     * @param boardManager
+     */
     public void setBoardManager(SudokuBoardManager boardManager) {
         this.boardManager = boardManager;
     }
 
+    /**
+     * Return gameStateFile.
+     *
+     * @return gameStateFile
+     */
     public String getGameStateFile() {
         return gameStateFile;
     }
 
+    /**
+     * Return tempGameStateFile.
+     *
+     * @return tempGameStateFile
+     */
     public String getTempGameStateFile() {
         return tempGameStateFile;
     }
 
+    /**
+     * Return boardManager.
+     *
+     * @return boardManager
+     */
     public SudokuBoardManager getBoardManager() {
         return boardManager;
     }
+
+    /**
+     * Return SudokuBoard.
+     *
+     * @return SudokuBoard
+     */
     public SudokuBoard getBoard(){
         return boardManager.getBoard();
     }
+
+    /**
+     * Return user.
+     *
+     * @return user
+     */
     public User getUser() {
         return user;
     }
+
+    /**
+     * Set up file.
+     */
     void setupFile(){
         if (!db.dataExists(user.getUsername(), GAME_NAME))
             db.addData(user.getUsername(), GAME_NAME);
         gameStateFile = db.getDataFile(user.getUsername(), GAME_NAME);
         tempGameStateFile = "temp_" + gameStateFile;
     }
+
+    /**
+     * Create cell buttons.
+     */
     void createCellButton(){
         SudokuBoard board = boardManager.getBoard();
         cellButtons = new ArrayList<>();
@@ -92,6 +193,10 @@ public class SudokuGameController {
             }
         }
     }
+
+    /**
+     * Update cell buttons.
+     */
     void updateCellButtons(){
         SudokuBoard board = boardManager.getBoard();
         int nextPos = 0;
@@ -107,6 +212,12 @@ public class SudokuGameController {
         }
     }
 
+    /**
+     * Return time in String format.
+     *
+     * @param time
+     * @return string format of time
+     */
     String convertTime(long time) {
         Integer hour = (int) (time / 3600000);
         Integer min = (int) ((time % 3600000) / 60000);
@@ -126,17 +237,32 @@ public class SudokuGameController {
         return hourStr + ":" + minStr + ":" + secStr;
     }
 
+    /**
+     * Return current score.
+     *
+     * @param totalTimeTaken
+     * @return score
+     */
     Integer calculateScore(Long totalTimeTaken) {
         int timeInSec = totalTimeTaken.intValue() / 1000;
         return 10000 / (timeInSec);
     }
 
+    /**
+     * Return whether update score succeed.
+     *
+     * @param score
+     * @return whether succeed
+     */
     boolean updateScore(int score){
         boolean result = user.updateScore(GAME_NAME, score);
         db.updateScore(user, GAME_NAME);
         return result;
     }
 
+    /**
+     * Load boardManager from file.
+     */
     public void loadFromFile() {
         try {
             InputStream inputStream = context.openFileInput(tempGameStateFile);
@@ -174,6 +300,11 @@ public class SudokuGameController {
         }
     }
 
+    /**
+     * Return whether board is solved.
+     *
+     * @return whether board is solved
+     */
     public boolean boardSolved() {
         return boardManager.boardSolved();
     }
